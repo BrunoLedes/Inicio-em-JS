0fc4326657fd307e76993712ba101e57a59a2ac3
BrunoLedes <brunonascimentoledes@gmail.com>
2023-05-16 18:28:12 -0300

    Exemplos aula 1

---
+++ type-string.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá Mundo!";
+const texto2= 'Ola mundo!';
+const senha = "senhaSupertSegura456!"
+const StringDeNumeros = "34567";
+
+const citacao = 'o Bruno disse "oi"';
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+// template string ou teplate literal 
