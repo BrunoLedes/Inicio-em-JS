3b759501570f97ca0d2621d0a2770789e7e49bde
BrunoLedes <brunonascimentoledes@gmail.com>
2023-06-01 16:14:52 -0300

    operadores e funcoes

---
+++ funcoes.js
@@ -0,0 +1,22 @@
+// let x = "";
+// console.log(x);
+// x = "oi;"
+
+// declara a funcão
+                   //string
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// execulta a função 1 ou + vezes
+imprimeTexto("oi");
+imprimeTexto("Teste Outro texto");
+imprimeTexto(soma());
+
+// tres formas de escrever funçoes
+
+function soma() {
+    return  2 + 2;
+}
+
+//console.log(soma());
--- type-boolean.js
+++ type-boolean.js
@@ -6,8 +6,10 @@ let cadastroAtivado = true
 
 // console.log(primeiroNumero === segundoNumero);
 
+let nome = window.prompt('teste')
 
 const texto1 = "Alura";
 const texto2 = "alura";
 
 console.log(texto1 === texto2);
+
